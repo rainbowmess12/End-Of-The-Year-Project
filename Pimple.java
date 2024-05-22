2f470e8d71926009133e350556f93af72a995204
hanvitlee <hanvlee27@bergen.org>
2024-05-22 13:26:12 -0400

    Version1
    Pimples class

---
+++ Pimple.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Pimple here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pimple extends Actor
+{
+    /**
+     * Act - do whatever the Pimple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Pimple(){
+        setImage("Pimple0.png");
+    }
+    
+    public void act()
+    {
+        setImage("Pimple1A.jpg");
+    }
+}
